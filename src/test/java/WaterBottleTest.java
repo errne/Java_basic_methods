37f3550435f4f736990f7b18d61edc2185803165
Ernest Fraiman <upapaus@gmail.com>
2019-01-07 16:14:47 +0000

    done water bottle task

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+    public class WaterBottleTest {
+        WaterBottle bottle;
+
+        @Before
+        public void before(){
+            bottle = new WaterBottle();
+        }
+
+        @Test
+        public void hasVolumeOf100(){
+            assertEquals(100, bottle.getVolume());
+        }
+
+        @Test
+        public void canDrink(){
+            bottle.drink();
+            assertEquals(90, bottle.getVolume());
+        }
+
+        @Test
+        public void canEmpty(){
+            bottle.empty();
+            assertEquals(0, bottle.getVolume());
+        }
+
+        @Test
+        public void canFill(){
+            bottle.drink();
+            bottle.fill();
+            assertEquals(100, bottle.getVolume());
+        }
+
+}
